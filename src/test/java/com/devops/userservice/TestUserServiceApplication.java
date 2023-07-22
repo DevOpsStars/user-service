3d33ca4b0928d2a162b025fc08301b796a8db27f
isidora-stanic <isidora.stanic@outlook.com>
2023-07-22 22:39:46 +0200

    fix: fixed db configuration so that tests can run containers with postgres

--- src/test/java/com/devops/userservice/TestUserServiceApplication.java
+++ src/test/java/com/devops/userservice/TestUserServiceApplication.java
@@ -2,9 +2,18 @@ package com.devops.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestUserServiceApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>("postgres:latest");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.from(UserServiceApplication::main).with(TestUserServiceApplication.class).run(args);

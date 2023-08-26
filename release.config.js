f90db444dfce0bfdb34d977058e2a9cf56a66da9
Mladenv7 <mladenvasic170@yahoo.com>
2023-08-26 15:04:15 +0200

    feat: add semantic release action to automate the release workflow

--- release.config.js
+++ release.config.js
@@ -1,16 +1,21 @@
-module.exports = {
-    branches: ['main', 'develop'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md'],
-          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-        },
-      ],
+const config = {
+  branches: ['main', 'develop'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+ [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
     ],
-  };
+    ["@semantic-release/git", {
+      "assets": ["dist/*.js", "dist/*.js.map", "docs/CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    '@semantic-release/github',
+    'semantic-release-github-actions-tags'
+  ]
+};
+
+module.exports = config;

a91b3fb4f943f8db7d388d9f07950b4fd06cb516
Mladenv7 <mladenvasic170@yahoo.com>
2023-09-10 23:53:07 +0200

    feat: finished implemeting authorization method

--- src/main/java/com/devops/userservice/controller/AuthenticationController.java
+++ src/main/java/com/devops/userservice/controller/AuthenticationController.java
@@ -18,10 +18,11 @@ import com.devops.userservice.dto.LoginRequestDTO;
 import com.devops.userservice.services.AuthenticationService;
 
 import javax.xml.transform.OutputKeys;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.Map.entry;
 
 @Controller
 @Validated
@@ -40,11 +41,49 @@ public class AuthenticationController {
     }
 
     @GetMapping("")
-    public ResponseEntity<?> authenticateUser(HttpServletRequest request, Authentication authentication){
+    public ResponseEntity<?> authorizeUser(HttpServletRequest request, Authentication authentication){
 
-        System.out.println(request.getHeader("originalURI"));
+        String requestURI = request.getHeader("originalURI");
 
-        if(authentication != null) return new ResponseEntity<>(null, HttpStatus.OK);
+        String role = authentication.getAuthorities().iterator().next().toString().substring(5);
+
+        List<List<String>> expectedRoles = new ArrayList<>(Arrays.asList(
+                new ArrayList<>(Arrays.asList("rating/api/host-ratings/new-rating/[0-9]+/[0-9]+/[1-5]", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("rating/api/host-ratings/update/[0-9]+/[1-5]", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("rating/api/host-ratings/delete/[0-9]+", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("rating/api/lodge-ratings/new-rating/[0-9]+/[0-9]+/[1-5]", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("rating/api/lodge-ratings/update/[0-9]+/[1-5]", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("rating/api/lodge-ratings/delete/[0-9]+", "ROLE_GUEST")),
+
+                new ArrayList<>(Arrays.asList("booking/api/reservations/[0-9]+/cancel", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("booking/api/reservations/[0-9]+/all-reservations", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("booking/api/requests/send-request", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("booking/api/requests/send-request/automatic-accept", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("booking/api/[0-9]+", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("booking/api/reservations/[0-9]+/cancel-count", "ROLE_HOST")),
+                new ArrayList<>(Arrays.asList("booking/api/reservations/lodges/active", "ROLE_HOST")),
+                new ArrayList<>(Arrays.asList("booking/api/[0-9]+/accept", "ROLE_HOST")),
+                new ArrayList<>(Arrays.asList("booking/api/[0-9]+/decline", "ROLE_HOST")),
+                new ArrayList<>(Arrays.asList("booking/api/[0-9]+/pending", "ROLE_HOST")),
+                new ArrayList<>(Arrays.asList("booking/api/reservations/[0-9]+/active", "ROLE_HOST", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("booking/api/reservations/logde/[0-9]+/active", "ROLE_HOST", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("booking/api/reservations/lodge/[0-9]+/period/count", "ROLE_HOST", "ROLE_GUEST")),
+                new ArrayList<>(Arrays.asList("booking/api/reservations/lodge/[0-9]+/period", "ROLE_HOST", "ROLE_GUEST")),
+
+                new ArrayList<>(Arrays.asList("lodging/api/availability/update/[0-9]+", "ROLE_HOST")),
+                new ArrayList<>(Arrays.asList("lodging/api/lodge/update/[0-9]+", "ROLE_HOST")),
+                new ArrayList<>(Arrays.asList("lodging/api/price/update/[0-9]+", "ROLE_HOST"))
+        ));
+
+        for(List<String> path : expectedRoles){
+            Pattern pattern = Pattern.compile(path.get(0), Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(requestURI);
+            if(matcher.find()){
+                for(int i = 1;i < path.size();++i){
+                    if(role.equals(path.get(i))) return new ResponseEntity<>(null, HttpStatus.OK);
+                }
+            }
+        }
 
         return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
